6c61db4ecb97620ebb398276ef3c33f596f36201
Dmytro Rumiantsev <drumian@wgu.edu>
2025-11-20 18:39:39 -0600

    temp function

---
+++ temp_convertion_function.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int power(int m);
+
+/* test power function */
+int main()
+{
+    int i;
+
+    for (i = 0; i < 100; ++i)
+        printf("%d %d %d\n", i, power(i), power(i));
+    return 0;
+}
+
+/* power:  raise base to n-th power; n >= 0 */
+int power(int far)
+{
+    
+
+    int i, c;
+    for(i=0; i< far; ++i)
+        c = 5 * (far - 32) / 9;
+    return c;
+}
