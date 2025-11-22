11bd1fb5e38037b72025cb21b2b9554723143409
Dmytro Rumiantsev <drumian@wgu.edu>
2025-11-22 13:15:43 -0600

    printing only lines over 20

---
+++ char_array.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#define MAXLINE 100    /* maximum input line size */
+
+int getline(char line[], int maxline);
+void copy(char to[], char from[]);
+
+/* print longest input line */
+int main()
+{
+    int len;            /* current line length */
+    int max;            /* maximum length seen so far */
+    char line[MAXLINE];     /* current input line */
+    char longest[MAXLINE];  /* longest line saved here */
+
+   
+    while ((len = getline(line, MAXLINE)) > 0){
+    printf("Current line: %d\n", len);
+        if (len > 20) {
+             printf("inside if");
+            copy(longest, line);
+             printf("This line is over 20: %s\n", line);
+
+        }
+    
+    }
+    return 0;
+   
+}
+
+/* getline:  read a line into s, return length */
+int getline(char s[], int lim)
+{
+   int c, i;
+
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+        s[i] = c;
+
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+
+    s[i] = '\0';
+    return i;
+}
+
+/* copy:  copy ′from′ into ′to′; assume to is big enough */
+void copy(char to[], char from[])
+{
+    int i;
+
+    i = 0;
+    while ((to[i] = from[i]) != '\0')
+        ++i;
+}
