c802eaa252219f2a1bc6877f31dff1b344b9007c
Rum1994 <dimarum7@gmail.com>
2025-11-30 15:34:31 -0600

    2_9 is done

---
+++ chapter_2/2_9.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int lower(int c);
+
+int main()
+{
+    printf("%c" , lower(getchar()));
+    return 0;
+}
+
+int lower(int c)
+{
+   
+    return (c>= 'A' && c <='Z') ? (c + 'a' - 'A') : c;
+}
+
